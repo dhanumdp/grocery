9325fcf69e44657eb658f57c9edb2033972c3c4d
Dhanu <dhanumca3748@gmail.com>
2020-04-16 18:10:03 +0530

    Started Building API

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var cors = require('cors');
 var mongoose = require('mongoose');
-
+const {User} = require('./models/users');
 var app = express();
 
 app.use(bodyParser.json());
@@ -31,6 +31,60 @@ mongoose.connect('mongodb://localhost:27017/GrocerySOS',{ useUnifiedTopology: tr
 });
 
 
-const userRoutes = require('./routes/userRoutes');
-app.use('/user',userRoutes);
+
+//Registration for all users
+app.post('/user/register',(req,res)=>{
+
+    const user = new User({
+    
+        fname : req.body.fname,
+        lname : req.body.lname,
+        email : req.body.email,
+        password : req.body.password,
+        role : req.body.role,
+        address : req.body.address,
+        contact : req.body.contact
+    })
+        
+    User.find({'email': req.body.email}).count((err,num)=>{
+        if(num != 0)
+        {
+         res.json({success:false, message:'Mail Id Exists. User Already Registered.'})
+        }
+        else
+        {
+            user.save((err,doc)=>{
+                if(err)
+                {
+                    res.json({success:false, message : 'Registration Failed '+err})
+                }
+                else
+                {
+                    res.json({success:true, message : 'User Registered Successfully'})
+                }
+            })
+        }
+
+
+    })
+});
+
+//for customer
+const customerRoutes = require('./routes/customerRoutes');
+app.use('/customer',customerRoutes);
+
+//for superAdmin
+const superAdminRoutes = require('./routes/superAdminRoutes');
+app.use('/superAdmin',superAdminRoutes);
+
+//for franchiseAdmin
+const franchiseAdminRoutes = require('./routes/franchiseAdminRoutes');
+app.use('/franchiseAdmin',franchiseAdminRoutes);
+
+//for vendorAdmin
+const vendorAdminRoutes = require('./routes/vendorAdminRoutes');
+app.use('/vendorAdmin',franchiseAdminRoutes);
+
+
+
 
--- routes/customerRoutes.js
+++ routes/customerRoutes.js
@@ -66,49 +66,6 @@ router.post('/login', (req,res)=>{
 
 
 
-//create Franchise
-
-/*
-
-    This route will work only after the user logged in and that user shoud be a SuperAdmin.
-
-    and these checkings will be done in FrontEnd by handling session.
-
-*/
-router.post('/createFranchise',(req,res)=>{
-     let franchise = new Franchise({
-        franchiseId : req.body.franchiseId,
-        pinCode : req.body.pinCode,
-        city: req.body.city,
-        district : req.body.district,
-        country : req.body.country,
-        franchiseName : req.body.franchiseName,
-        createdBy : req.body.createdBy, // In front end it comes through session variable,
-        franchiseAdmins : req.body.franchiseAdmins
-    })
-
-   Franchise.find({'pinCode': req.body.pinCode}).count((err,num)=>{
-       if(num != 0)
-       {
-        res.json({success:false, message:'Franchise Exists'})
-       }
-       else
-       {
-           franchise.save((error,doc)=>{
-            if(error)
-            {
-                res.json({success:false, message : 'Franchise Creation Failed '+error})
-            }
-            else
-            {
-                res.json({success:true, message : 'Franchise Created Successfully'})
-            }
-        })
-       }
-   })
-})
-
-
 
 
 module.exports = router;
